a93b77fe57a578729f9f695ea8c6ea12f1013c10
agustin123gd <agustin.gutierrez.103@gmail.com>
2021-10-08 13:03:01 -0500

    login

---
+++ login/envioDePeticionesLogin.js
@@ -0,0 +1,18 @@
+var btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener("click", function () {
+    var params = new URLSearchParams();
+    var email = document.getElementById("exampleInputEmail1").value;
+    var pass = document.getElementById("exampleInputPassword1").value;
+    var cont = document.getElementById("cont");
+
+    params.append("nombre", email);
+    params.append("pass", pass);
+    axios.get("http://localhost:4567/registro?"+params)
+        .then(function (rs) {
+            cont.innerHTML=rs.data;
+            console.log(rs.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
